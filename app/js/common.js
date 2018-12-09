3c1c0164b38daf65a0106edc1f3d6e70181868a9
Dmitry Ershov <degalweb@icloud.com>
2018-12-10 00:27:45 +0300

    Hamburger menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,19 @@
 $(function() {
 
-	// Custom JS
+	$('#my-menu').mmenu({
+		extensions: ['widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black', 'position-right'],
+		navbar: {
+			title: '<img src="img/logo-1.svg" alt="Салон красоты S&Mitler">'
+		},
+		offCanvas: {
+			position: false
+		},
+	});
+	var api = $('#my-menu').data('mmenu');
+	api.bind('open:start', function(){
+		$('.hamburger').addClass('is-active')
+	}).bind('close:finish', function() {
+		$('.hamburger').removeClass('is-active');
+	})
 
 });
